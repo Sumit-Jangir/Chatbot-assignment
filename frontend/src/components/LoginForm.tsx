38945798fb43ea1bddebea354bff9cb5c9be802e
Sumit Jangir <jangirsumit1011@gmail.com>
2025-02-07 16:26:35 +0530

    add login button animation

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -90,7 +90,17 @@ export default function LoginForm({ setShowForm, setFormSubmit }: LoginFormProps
         {/* {message && <p className="text-center text-sm">{message}</p>} */}
 
         <Button type="submit" className="w-full bg-black hover:bg-gray-800" disabled={loading}>
-          {loading ? "Logging in..." : "Login"}
+          {/* {loading ? "Logging in..." : "Login"} */}
+          {loading ? (<div className="flex justify-start">
+            <div className="bg-transparent rounded-lg p-4">
+              <div className="flex space-x-1">
+                <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" />
+                <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:0.2s]" />
+                <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce [animation-delay:0.4s]" />
+              </div>
+            </div>
+          </div>
+) : "Login"}
         </Button>
       </form>
     </Card>
